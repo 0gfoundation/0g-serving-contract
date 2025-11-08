7c8262ffd8d392dc9b2c477c438a05ef1757578e
Ravenyjh <30866879+Ravenyjh@users.noreply.github.com>
2025-11-08 13:37:25 +0800

    feat: add Mainnet configuration to Hardhat for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,7 @@ import { HardhatUserConfig } from "hardhat/config";
 dotenv.config();
 
 const ZG_TESTNET_V4_PRIVATE_KEY = process.env.ZG_TESTNET_V4_PRIVATE_KEY || "";
+const ZG_MAINNET_PRIVATE_KEY = process.env.ZG_MAINNET_PRIVATE_KEY || "";
 
 import "./src/tasks/upgrade";
 
@@ -77,6 +78,12 @@ const config: HardhatUserConfig = {
             url: "https://evmrpc-testnet.0g.ai",
             accounts: [ZG_TESTNET_V4_PRIVATE_KEY],
             chainId: 16602,
+            gasPrice: 12000000000,
+        },
+        zgMainnet: {
+            url: "https://evmrpc.0g.ai",
+            accounts: [ZG_MAINNET_PRIVATE_KEY],
+            chainId: 16661,
             gasPrice: 12000000000,
         },
     },
